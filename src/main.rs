659cd3fbb94b1a18f0ec015fc09edc873a57530a
Prateek <pranav.chiddarwar@gitlab.com>
2023-12-06 18:46:04 +0530

    Wrong answer

--- src/main.rs
+++ src/main.rs
@@ -20,11 +20,9 @@ fn main() {
     let num_string_map = num_val_map.iter().map(|(val, key)| (key.to_string(), *val)).collect::<HashMap<String, u32>>();
     let num_rev_string_map = num_val_map.iter().map(|(val, key)| (key.chars().rev().collect::<String>(), *val)).collect::<HashMap<String, u32>>();
 
-    println!("{0:?}", num_string_map);
-    println!("{0:?}", num_rev_string_map);
-
     let mut ans: u32 = 0;
     content.lines().for_each(|line| {
+        println!("{0}", line);
         let first_num = fetch_first_num(line.to_string() , &num_string_map);
         let last_num = fetch_first_num(line.chars().rev().collect::<String>(), &num_rev_string_map);
 
@@ -36,20 +34,25 @@ fn main() {
 }
 
 fn fetch_first_num(line: String, map: &HashMap<String, u32>) -> Option<u32> {
-    let vec_of_words = map.keys().map(|ele| ele.as_str()).collect::<Vec<&str>>();
-    let word = get_word(&line, vec_of_words);
-    // if let Some(occurance) = map.keys().find(|&s| line.contains(s)) {
-    //     println!("{0}", occurance);
-    // }
-    match word {
-        Some(w) => todo!(),
-        None => None,
+    let vec_of_words = map.keys().map(String::as_str).collect::<Vec<&str>>();
+    for (i, c) in line.char_indices() {
+        // if c.is_digit(10) {return c.to_digit(10)};
+        if let Some(digit) = c.to_digit(10) {
+            // println!("Number: {0}", digit);
+            return Some(digit);
+        }
+        let mut query = c.to_string();
+        let mut vec = vec_of_words.clone();
+        while !vec.is_empty() {
+            vec.retain(|&ele| ele.starts_with(&query));
+            if let Some(word) = vec.get(0) {
+                if i + word.len() <= line.len() && &line[i..i + word.len()] == *word {
+                    // println!("word: {0}", word);
+                    return map.get(*word).cloned();
+                }
+            }
+            query.push(line.chars().nth(i + query.len())?);
+        }
     }
-}
-
-fn get_word(line: &str, vec_of_words: Vec<&str>) -> Option<&str> {
-    line.chars().skip_while(|ele| {
-    
-    });
     None
 }
