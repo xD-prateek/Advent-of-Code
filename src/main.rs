df18d183e1ce1ed6ae69a18ab4c641aef494f0e7
pn00@dearch <prateek.n2000@ymail.com>
2023-12-11 02:59:46 +0530

    Used BTreeSet approach because had binary searching in mind. But ig it is not used here. Anyways, the solution works!

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,50 @@
-use std::fs::read_to_string;
+use std::{fs::read_to_string, collections::BTreeSet};
+
+#[derive(Eq)]
+// representing destination range start, source range start, range length
+struct Map(u64, u64, u64);
+
+impl PartialOrd for Map {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl Ord for Map {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        self.1.cmp(&other.1)
+    }
+}
+
+impl PartialEq for Map {
+    fn eq(&self, other: &Self) -> bool {
+        (self.1..self.2).contains(&other.2)
+    }
+}
 
 fn main() {
     let file_name: &str = "src/input.txt";
-    let content = read_to_string(file_name).unwrap();
+    let almanac = read_to_string(file_name).unwrap();
+    let s = almanac.lines().collect::<Vec<&str>>().split(|&line| line.is_empty()).map(|l| l.to_vec()).collect::<Vec<Vec<&str>>>();
+    let mut content_iter = s.into_iter();
+    let seeds = content_iter.next().map(|s| s.get(0).unwrap_or_else(|| panic!("seeds not found...")).split(':').nth(1).unwrap_or_else(|| panic!("error reading seeds...")).split_whitespace().map(|num| num.parse::<u64>().unwrap()).collect::<Vec<u64>>()).unwrap();
+    // println!("{0:?}", seeds);
 
-    content.lines().for_each(|line| println!("{0}", line));
+    let mapping = content_iter.map(|v| {
+        v.into_iter().skip(1).fold(BTreeSet::new(), |mut acc, m| {
+            let mut map_iter = m.split_whitespace().map(|v| v.parse::<u64>().unwrap());
+            acc.insert(Map(map_iter.next().unwrap(), map_iter.next().unwrap(), map_iter.next().unwrap()));
+            acc
+        })
+    }).collect::<Vec<BTreeSet<Map>>>();
+
+    let ans = seeds.into_iter().map(|seed| {
+        mapping.iter().fold(seed, |source, m| {
+           match m.into_iter().find(|&ele| source >= ele.1 && source < ele.1 + ele.2) {
+                Some(ele) => source - ele.1 + ele.0,
+                None => source,
+            }
+        })
+    }).min().unwrap();
+    println!("ANS: {0}", ans);
 }
