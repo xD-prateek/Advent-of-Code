61591ca08d82f776bf1d45e5c5c2939b8d9578db
Prateek <pranav.chiddarwar@gitlab.com>
2023-12-19 18:49:34 +0530

    unfinished changes

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,46 @@
-use std::fs::read_to_string;
+use std::{ fs::read_to_string, fmt::Display };
 
 fn main() {
     let file_name: &str = "src/input.txt";
     let content = read_to_string(file_name).unwrap();
 
-    content.lines().for_each(|line| println!("{0}", line));
+    let game = content.lines().map(|l| {
+        let mut line_iter = l.split_whitespace().take(2);
+        (Hand::new(line_iter.next().unwrap()), line_iter.next().unwrap().parse::<u32>().unwrap())
+    }).collect::<Vec<(Hand, u32)>>().sort_by(|a, b| a.0.cmp(&b.0));
+
+    println!("{0:?}", game);
+}
+
+#[derive(Eq, Debug)]
+struct Hand(String);
+
+impl PartialOrd for Hand {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl Ord for Hand {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+       todo!(); 
+    }
+}
+
+impl PartialEq for Hand {
+    fn eq(&self, other: &Self) -> bool {
+        self.0 == other.0
+    }
+}
+
+impl Hand {
+    fn new(hand: &str) -> Self {
+        Hand(hand.to_string())
+    }
+}
+
+impl Display for Hand {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{0}", self.0)
+    }
 }
