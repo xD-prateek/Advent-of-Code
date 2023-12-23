ceca9a8c0aae829d01b1a9729aa52f5de45e2d4c
pn00@dearch <prateek.n2000@ymail.com>
2023-12-23 14:32:11 +0530

    Completed!

--- src/main.rs
+++ src/main.rs
@@ -4,5 +4,18 @@ fn main() {
     let file_name: &str = "src/input.txt";
     let content = read_to_string(file_name).unwrap();
 
-    content.lines().for_each(|line| println!("{0}", line));
+    let ans = content.lines().fold(0i32, |acc, line| {
+        let history = line.split_whitespace().map(|num| num.parse::<i32>().unwrap_or_default()).collect::<Vec<i32>>();
+        let extrapolate = get_next(history);
+        acc + extrapolate
+    });
+
+    println!("ANS: {0}", ans);
+}
+
+fn get_next(reading: Vec<i32>) -> i32 {
+    match reading.iter().all(|&num| num == 0) {
+        true => 0,
+        false => reading.last().unwrap() + get_next(reading.windows(2).map(|ele| ele[1] - ele[0]).collect::<Vec<i32>>()),
+    }
 }
