462e0ac96eccc66552880aa37f0dead771be7f9e
pn00@dearch <prateek.n2000@ymail.com>
2023-12-23 11:13:33 +0530

    Changed approach. Completed!

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ fn main() {
 
     let mut line_iter = content.lines();
 
-    let mut directions_iter = line_iter.next().unwrap().chars().filter_map(|c| Dir::new_from_str(c)).cycle();
+    let directions_iter = line_iter.next().unwrap().chars().filter_map(|c| Dir::new_from_str(c)).cycle();
 
     let map: HashMap<&str, (&str, &str)> = line_iter.filter(|l| !l.is_empty()).fold(HashMap::new(), |mut acc, l| {
         let mut kv_iter = l.split('=');
@@ -18,30 +18,41 @@ fn main() {
     });
     // println!("{0:#?}", map);
 
-    let mut next = map.keys().filter_map(|&key| {
+    let next = map.keys().filter_map(|&key| {
         match key.ends_with('A') {
             true => Some(key),
             false => None,
         }
     }).collect::<Vec<&str>>();
 
-    let mut ans = 0u32;
-    while !next.iter().all(|val| val.ends_with('Z')) {
-        let direction = directions_iter.next().unwrap();
-        println!("{0:?}. Now going {1}!", next, match direction {
-        R => "right",
-        L => "left",
-        });
-        use Dir::{R, L};
-        next.iter_mut().for_each(|key| {
-            *key = match &direction {
-                L => map.get(key).unwrap().0,
-                R => map.get(key).unwrap().1,
+    let mut lcm = 1u64;
+    next.into_iter().for_each(|start| {
+        let mut directions = directions_iter.clone();
+        let mut first_z = None;
+        let mut current = start;
+        let mut steps = 0u64;
+        loop {
+            use Dir::{ R, L };
+            current = match directions.next().unwrap() {
+                R => map.get(current).unwrap().1,
+                L => map.get(current).unwrap().0,
             };
-        });
-        ans += 1;
-    }
-    println!("ANS: {0}", ans);
+
+            if current.ends_with('Z') {
+                if first_z.is_none() {
+                    first_z = Some(current);
+                    steps = 0;
+                }
+                else if current == first_z.unwrap_or_default() {
+                    lcm = lcm * steps / gcd(steps, lcm);
+                    break;
+                }
+            }
+            steps += 1;
+        }
+    });
+
+    println!("ANS: {0}", lcm);
 }
 
 enum Dir {
@@ -56,5 +67,12 @@ impl Dir {
             'L' => Some(Self::L),
             _ => None,
         }
+    }
+}
+
+fn gcd(a: u64, b: u64) -> u64 {
+    match a {
+        0 => b,
+        _ => gcd(b % a, a),
     }
 }
