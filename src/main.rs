009bc8cf653d6ed63f8705c0fb58a2cf8186cc05
pn00@dearch <prateek.n2000@ymail.com>
2023-12-30 19:04:40 +0530

    Completed!

--- src/main.rs
+++ src/main.rs
@@ -6,5 +6,6 @@ fn main() {
     let file_name: &str = "src/input.txt";
     let content = read_to_string(file_name).unwrap();
     // let ans = Map::from(content).get_enclosed_area();
-   Map::from(content).get_enclosed_area();
+   let ans = Map::from(content).get_enclosed_area();
+   println!("ANS: {0}", ans);
 }
--- src/map.rs
+++ src/map.rs
@@ -1,6 +1,8 @@
 pub struct Map {
     map: Vec<Vec<char>>,
     start: Option<(usize, usize)>,
+    y_limits: (usize, usize),
+    x_limits: (usize, usize),
 }
 
 impl From<String> for Map {
@@ -12,15 +14,29 @@ impl From<String> for Map {
             }
             ch
         }).collect::<Vec<char>>()).collect::<Vec<Vec<char>>>();
-        Self {
-            map,
-            start,
+        match start {
+            Some(s) => Self {
+                map,
+                start,
+                y_limits:(s.0, s.0),
+                x_limits: (s.1, s.1),
+            },
+            None => {
+             let y_limits = (0, map.len());
+             let x_limits = (0, map.get(0).unwrap().len());
+             Self {
+                map,
+                start,
+                y_limits,
+                x_limits,
+            }
         }
     }
 }
+}
 
 impl Map {
-    pub fn get_pipe_coordinates(&self) -> Vec<(usize, usize)> {
+    fn get_pipe_coordinates(&mut self) -> Vec<Info> {
         match self.start {
             None => vec!{},
             Some(start) => {
@@ -34,15 +50,16 @@ impl Map {
                 }
                 assert!(next_coordinate.is_some(), "Loop not present in input.");
 
-                let mut path: Vec<(usize, usize)> = vec!{ start };
                 let mut previous_coordinate = start;
                 let nc = next_coordinate.unwrap();
                 let mut current_coordinate = nc.0;
                 let mut current_char = nc.1;
+                let mut path = vec!{ Info(start, 'S') };
 
                 while 'S' != current_char {
-                    println!("previous coordinate: {1:?}, current coordinate: {0:?}, current_char: {2}", previous_coordinate, current_coordinate, current_char);
-                    path.push(current_coordinate);
+                    self.y_limits = (current_coordinate.0.min(self.y_limits.0), current_coordinate.0.max(self.y_limits.1));
+                    self.x_limits = (current_coordinate.1.min(self.x_limits.0), current_coordinate.1.max(self.x_limits.1));
+                    path.push(Info(current_coordinate, current_char));
                     let dir = match Pipe::new(current_char).unwrap() {
                         Pipe::Horizontal => match previous_coordinate.1 + 1 == current_coordinate.1 {
                             true => Direction::East,
@@ -139,10 +156,25 @@ impl Map {
         }
     }
 
-    pub fn get_enclosed_area(&self) {
+    pub fn get_enclosed_area(&mut self) -> u32 {
        let pipe = self.get_pipe_coordinates(); 
-       println!("Coordinates: {0:?}", pipe);
-    }
+
+       let no_of_enclosed_points = (self.y_limits.0 + 1..self.y_limits.1).flat_map(|y| (self.x_limits.0 + 1..self.x_limits.1).map(move |x| (y, x))).filter(|&ele| pipe.iter().find(|info| info.0 == ele).is_none()).fold(0u32, |acc, (y, x)| {
+        let mut passes = pipe.iter().filter(|info| x == info.0.1 && y > info.0.0 && '|' != info.1).peekable();
+        let mut count = 0u32;
+        while let Some(info) = passes.next() {
+            match info.1 {
+                '-' => count += 1,
+                _ => count += match (info.1, passes.next().map(|info| info.1)) {
+                    ('J', Some('F')) | ('7', Some('L')) | ('F', Some('J')) | ('L', Some('7')) => 1,
+                    _ => 0,
+                },
+            }
+        }
+        acc + (count & 1)
+    });
+       no_of_enclosed_points
+   }
 }
 
 struct Info((usize, usize), char);
