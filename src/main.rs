5a39ebb7614f1737151d0e8d27a89b5091e4743f
pn00@dearch <prateek.n2000@ymail.com>
2024-01-03 01:51:35 +0530

    changed approach

--- src/main.rs
+++ src/main.rs
@@ -6,33 +6,42 @@ fn main() {
 
     let ans = content.lines().fold(0u32, |acc, line| {
         let mut record_iter = line.split_whitespace().into_iter();
-        let (spring_map, contiguous_springs) = (record_iter.next().unwrap_or_else(|| panic!("Input error! Spring map not found.")), record_iter.next().unwrap_or_else(|| panic!("Input error! Contiguous spring array not found.")).split(',').map(|c| c.parse::<usize>().unwrap_or_else(|_| panic!("Error parsing. Invalid Input!"))).collect::<Vec<usize>>());
+        let (spring_map, contiguous_springs) = (String::from(record_iter.next().unwrap_or_else(|| panic!("Input error! Spring map not found."))), record_iter.next().unwrap_or_else(|| panic!("Input error! Contiguous spring array not found.")).split(',').map(|c| c.parse::<usize>().unwrap_or_else(|_| panic!("Error parsing. Invalid Input!"))).rev().collect::<Vec<usize>>());
 
-        acc + permutations(spring_map, contiguous_springs)
+        acc + permutations(&spring_map, contiguous_springs)
     });
 
     println!("ANS: {0}", ans);
 }
 
 fn permutations(row_of_springs: &str, mut contiguous_springs: Vec<usize>) -> u32 {
-    if contiguous_springs.is_empty() {
-        return 0;
-    }
-    let min_len = contiguous_springs.iter().sum::<usize>() + contiguous_springs.len() - 1;
-    if min_len > row_of_springs.len() {
-        return 0;
-    }
-    let first_contiguous_springs_count = contiguous_springs.remove(0);
-    let potential_len = min_len - first_contiguous_springs_count - 1;
-    let max_split_at = row_of_springs.len() - potential_len - row_of_springs.chars().rev().skip(potential_len).take_while(|&c| c == '#').count();
-    (first_contiguous_springs_count..max_split_at).fold(0u32, |acc, i| {
-        let (a, b) = row_of_springs.split_at(i + 1);
-        let mut a = (&a[a.len() - first_contiguous_springs_count - 1..]).chars().rev();
-        let b = b.trim_start_matches(|c| c == '.');
-
-        match a.next().map_or(false, |last| last == '.' || last == '?') && a.all(|c| c == '#' || c == '?') {
-            true => acc + permutations(b, contiguous_springs.clone()),
-            false => acc,
+    // trim '.'; this will reduce the recursive calls
+    let row_of_springs = row_of_springs.trim_start_matches(|c| c == '.');
+    if let Some(matched) = contiguous_springs.pop() {
+        let mut str_iter = row_of_springs.chars();
+        if str_iter.by_ref().take(matched).all(|c| c != '.') {
+            if let Some(period) = str_iter.next() {
+                if period == '#' {
+                   contiguous_springs.push(matched);
+                   permutations(&row_of_springs[1..], contiguous_springs)
+                }
+                else {
+                    // continue by splitting the remaining string
+                    permutations(&row_of_springs[matched + 1..], contiguous_springs)
+                }
+            }
+            else {
+                1
+            }
         }
-    })
+        else {
+            // call permutations with the remaining string
+            contiguous_springs.push(matched);
+            permutations(&row_of_springs[1..], contiguous_springs)
+        }
+
+    }
+    else {
+        0
+    }
 }
