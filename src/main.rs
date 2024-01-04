1490b57ceca2710e9f8373a72320211b1cd2d434
Prateek <pranav.chiddarwar@gitlab.com>
2024-01-04 19:53:20 +0530

    unfinished commit

--- src/main.rs
+++ src/main.rs
@@ -16,48 +16,25 @@ fn main() {
 
 fn permutations(row_of_springs: &str, contiguous_springs: &mut Vec<usize>) -> u32 {
     println!("row_of_springs: {0}, contiguous_springs: {1:?}", row_of_springs, contiguous_springs);
-    let row_of_springs = row_of_springs.trim_start_matches(|c| c == '.');
-    if row_of_springs.is_empty() {
-        return 0;
-    }
-    match contiguous_springs.pop() {
-        None => 0,
-        Some(to_match) => {
-            if contiguous_springs.is_empty() {
-                return match row_of_springs.contains('#') {
-                    true => 0,
-                    false => 1,
-                };
+    let matched = contiguous_springs.pop().unwrap();
+    println!("To match: {0}", matched);
+    let mut springs = row_of_springs.char_indices();
+    // put in loop
+    let mut ans = 0u32;
+    let permute = match contiguous_springs.iter().sum::<usize>() - contiguous_springs.len() {
+        0 => 0,
+        n => n - 1,
+    };
+    for i in 0..permute {
+        let springs_iter = springs.by_ref();
+        if springs_iter.skip(i).take(matched).all(|(_, c)| c != '.') {
+            println!("MATCHED!");
+            // its is a valid match
+            match springs_iter.next() {
+                Some((i, c)) if c != '#' && !contiguous_springs.is_empty() => ans += permutations(&row_of_springs[i + 1..], contiguous_springs),
+                _ => return 1,
             }
-            let mut springs_iter = row_of_springs.char_indices();
-            let mut combinations = 0u32;
-            for _ in 0..contiguous_springs.iter().sum::<usize>() + contiguous_springs.len() - 1 {
-                for i in 0..to_match {
-                    match springs_iter.next() {
-                        Some((_, c)) => {
-                            if c == '.' {
-                                contiguous_springs.push(to_match);
-                                combinations += permutations(&row_of_springs[i..], contiguous_springs);
-                                break;
-                            }
-                        },
-                        None => return 0,
-                    }
-                }
-                if let Some((i, c)) = springs_iter.next() {
-                    if c != '#' {
-                        combinations += permutations(&row_of_springs[i..], contiguous_springs);
-                        break;
-                    }
-                    else {
-                        panic!("Invalid input. Springs did not match.");
-                    }
-                }
-                else {
-                    return 1;
-                }
-            }
-            combinations
-        },
+        }
     }
+    ans
 }
