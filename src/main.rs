e2b4636611d6fe85ff3a0da91551d53256c64e51
pn00@dearch <prateek.n2000@ymail.com>
2024-01-10 00:32:55 +0530

    completed!

--- src/main.rs
+++ src/main.rs
@@ -4,32 +4,31 @@ fn main() {
     let file_name: &str = "src/input.txt";
     let content = read_to_string(file_name).unwrap();
 
-    let ans = content.split_terminator("\n\n").fold(0usize, |acc, s| {
-        // let a = get_above_rows(s.1);
-        // let p = get_previous_columns(s.1);
-        // if a != 0 && p != 0 {
-        //     println!("{0}", s.1);
-        //     println!("a: {0}, p: {1}, at {2}\n", a, p, s.0);
-        // }
-        // 0
-        acc + 100 * get_above_rows(s) + get_previous_columns(s)
-    });
+    let ans = content.split_terminator("\n\n").fold(0usize, |acc, s| acc + 100 * get_above_rows(s) + get_previous_columns(s));
     println!("ANS: {0}", ans);
 }
 
 fn get_previous_columns(valley: &str) -> usize {
-    let mut lines = valley.lines();
-    let p = lines.next().unwrap().char_indices().collect::<Vec<(usize, char)>>().windows(2).filter(|&s| s[0].1 == s[1].1).map(|s| (s[0].0, s[1].0)).collect::<Vec<(usize, usize)>>();
-    let split = lines.fold(p, |mut acc, line| {
-        acc.retain(|c| {
-            let (left, right) = line.split_at(c.0);
+    let mut lines_iter = valley.lines();
+    let potential_mirrors = {
+        let first_line = lines_iter.next().unwrap();
+        (1..first_line.len()).filter(|&i| {
+            let left = &first_line[..i];
+            let right = &first_line[i..];
+            right.chars().zip(left.chars().rev()).all(|(a, b)| a == b)
+        }).collect::<Vec<usize>>()
+    };
+    let mirror = lines_iter.fold(potential_mirrors, |mut acc, line| {
+        acc.retain(|&i| {
+            let left = &line[..i];
+            let right = &line[i..];
             right.chars().zip(left.chars().rev()).all(|(a, b)| a == b)
         });
         acc
     });
-    println!("split: {0:?}", split);
-    match split.get(0) {
-        Some(k) if split.len() == 1 => k.1,
+
+    match mirror.len() {
+        1 => mirror.get(0).unwrap().to_owned(),
         _ => 0,
     }
 }
@@ -47,7 +46,7 @@ fn get_above_rows(valley: &str) -> usize {
 
     potential_lines.into_iter().fold(0usize, |acc, i| {
         let valley_of_lines = valley.lines().collect::<Vec<&str>>();
-        let top = &valley_of_lines[0..i];
+        let top = &valley_of_lines[..i];
         let bottom = &valley_of_lines[i..];
 
         match bottom.into_iter().zip(top.into_iter().rev()).all(|(&a, &b)| a == b) {
