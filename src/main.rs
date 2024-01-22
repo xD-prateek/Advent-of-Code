26a33b4f5e4e7c8f43c5aef916e4a0d91e305b6f
pn00@dearch <prateek.n2000@ymail.com>
2024-01-22 14:08:33 +0530

    Added error if input not readable

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use std::fs::read_to_string;
 
 fn main() {
     let file_name: &str = "src/input.txt";
-    let content = read_to_string(file_name).unwrap();
+    let content = read_to_string(file_name).unwrap_or_else(|_| panic!("Error reading input."));
 
     content.lines().for_each(|line| println!("{0}", line));
 }
