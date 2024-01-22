da7070235675b1df88c707e12fb745ab2f17cb71
pn00@dearch <prateek.n2000@ymail.com>
2024-01-22 14:39:36 +0530

    bruhh..very few lines. Completed!

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,14 @@ use std::fs::read_to_string;
 
 fn main() {
     let file_name: &str = "src/input.txt";
-    let content = read_to_string(file_name).unwrap_or_else(|_| panic!("Error reading input."));
+    let steps = read_to_string(file_name).unwrap_or_else(|_| panic!("Error reading input."));
 
-    content.lines().for_each(|line| println!("{0}", line));
+    let ans = steps.split(',').fold(0usize, |acc, step| acc + encode(step));
+    println!("ANS: {ans}");
+}
+
+fn encode(input: &str) -> usize {
+    input.chars().fold(0usize, |current_value, c| {
+        (current_value + c as u8 as usize) * 17 % 256
+    })
 }
