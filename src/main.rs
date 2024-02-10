3c99a70d30cdbb703894b6288676e1d365d4cffa
pn00@dearch <prateek.n2000@ymail.com>
2024-02-10 15:35:35 +0530

    wrong answer

--- src/main.rs
+++ src/main.rs
@@ -6,13 +6,10 @@ fn main() {
 
     let stack = Stack::new_from_str(&content);
 
-    // if stack.assert_mono_width() {
-    //     println!("All have width of 1");
-    // }
-    // else {
-    //     println!("Some have width of 2");
-    // }
-    println!("stack: {stack:#?}");
+    // println!("stack: {stack:?}");
+    let ans = stack.get_bricks_safe_to_disintegrate();
+
+    println!("ANS: {ans}");
 }
 
 #[derive(Debug)]
@@ -20,39 +17,42 @@ struct Stack (Vec<Brick>);
 
 impl Stack {
     fn new_from_str(content: &str) -> Self {
-        let mut cache: HashMap<(usize, usize), usize> = HashMap::new();
-        let stack = content.lines().fold(Vec::new(), |mut acc: Vec<Brick>, line| {
-            let mut brick = Brick::new_from_str(line);
-            // determine the stabilized position of brick 
+        let mut stack = content.lines().map(Brick::new_from_str).collect::<Vec<Brick>>();
+        stack.sort_by_key(|brick| brick.0.2);
+        stack.iter_mut().fold(HashMap::new(), |mut cache: HashMap<(usize, usize), usize>, brick| {
             let coordinates = brick.get_contained_xy_coordinates();
-            // println!("coordinates for {brick:?} are {coordinates:?}");
-            let z = coordinates.iter().map(|coor| cache.get(coor).copied().unwrap_or(1)).max().unwrap_or_else(|| panic!("error finding max."));
-            // set brick values
-            println!("max z coor: {z}");
+            let z = coordinates.iter().map(|coor| cache.get(coor).copied().unwrap_or(0)).max().unwrap_or_else(|| panic!("error finding max.")) + 1;
             brick.set_z_coordinate(z);
-            // add to cache
             coordinates.into_iter().for_each(|coor| { cache.insert(coor, z); });
-            acc.push(brick);
-            acc
+            cache
         });
+
         Self(stack)
     }
 
-    fn assert_mono_width(&self) -> bool {
-        self.0.iter().fold(true, |acc , brick| {
-            let count =  [brick.0.0 as isize - brick.1.0 as isize, brick.0.1 as isize - brick.1.1 as isize, brick.0.2 as isize - brick.1.2 as isize].into_iter().filter(|&e| e != 0).count();
-            let b = match count {
-                0 => {
-                    println!("all matched for brick: {brick:?}");
-                    false
-                }
-                1 => true,
-                _ => {
-                    println!("two did not match for brick: {brick:?}");
-                    false
-                }
-            };
-            acc && b
+    fn get_bricks_safe_to_disintegrate(&self) -> usize {
+        let mut supports = vec!{ Vec::new(); self.0.len() };
+        let mut supported_by_len = vec!{ 0; self.0.len() };
+        // self.0.iter().enumerate().for_each(|(j, upper_brick)| {
+        //     self.0.iter().enumerate().take(j).filter(|(_, lower_brick)| lower_brick.overlaps(upper_brick) && upper_brick.0.2 == lower_brick.1.2 + 1).for_each(|(i, _)| {
+        //         supports.get_mut(j).unwrap_or_else(|| panic!("index out of bounds. {j}")).push(i);
+        //         *supported_by_len.get_mut(i).unwrap_or_else(|| panic!("index out of bounds. {i}")) += 1;
+        //     });
+        // });
+        self.0.iter().enumerate().for_each(|(i, lower_brick)| {
+            self.0.iter().enumerate().skip(i).filter(|(_, upper_brick)| upper_brick.overlaps(lower_brick) && upper_brick.0.2 == lower_brick.1.2 + 1).for_each(|(j, _)| {
+                supports.get_mut(i).unwrap_or_else(|| panic!("index out of bounds. {j}")).push(j);
+                *supported_by_len.get_mut(j).unwrap_or_else(|| panic!("index out of bounds. {j}")) += 1;
+            });
+        });
+        // println!("supports: {supports:?}");
+        // println!("supported_by_len: {supported_by_len:?}");
+
+        (0..self.0.len()).fold(0, |acc, i| {
+            match supports.get(i).unwrap_or_else(|| panic!("index out of bounds. {i}")).iter().all(|&j| supported_by_len.get(j).unwrap_or_else(|| panic!("index out of bounds. {j}")) > &1) {
+                true => acc + 1,
+                false => acc,
+            }
         })
     }
 }
@@ -62,26 +62,28 @@ struct Brick(Coordinate, Coordinate);
 
 impl Brick {
     fn new_from_str(brick: &str) -> Self {
-        let mut brick_iter = brick.split('~').map(|b| Coordinate::new_from_str(b));
-        Self(brick_iter.next().unwrap_or_else(|| panic!("error reading brick.")), brick_iter.next().unwrap_or_else(|| panic!("error reading brick.")))
+        let mut coordinate_iter = brick.split('~').map(Coordinate::new_from_str);
+        let first_coor = coordinate_iter.next().unwrap_or_else(|| panic!("error reading brick."));
+        let second_coor = coordinate_iter.next().unwrap_or_else(|| panic!("error reading brick."));
+        assert!(first_coor.2 <= second_coor.2);
+        Self(first_coor, second_coor)
+    }
+
+    fn overlaps(&self, other: &Self) -> bool {
+        self.0.0.max(other.0.0) <= self.1.0.min(other.1.0) && self.0.1.max(other.0.1) <= self.1.1.min(other.1.1)
     }
 
     fn get_contained_xy_coordinates(&self) -> Vec<(usize, usize)> {
         (self.0.0..=self.1.0).flat_map(|x| (self.0.1..=self.1.1).map(move |y| (x, y))).collect::<Vec<(usize, usize)>>()
     }
 
-    fn get_top_point(&self) -> usize {
-        self.0.2.max(self.1.2)
-    }
-
     fn set_z_coordinate(&mut self, z: usize) {
-        let min = self.0.2.min(self.1.2);
-        if self.0.2 == min {
-            self.0.2 = z;
-        }
-        if self.1.2 == min {
-            self.0.2 = z;
-        }
+        let (lower, upper) = match self.0.2 < self.1.2 {
+            true => (&mut self.0.2, &mut self.1.2),
+            false => (&mut self.1.2, &mut self.0.2),
+        };
+        *upper = *upper + z - *lower;
+        *lower = z;
     }
 }
 
@@ -93,4 +95,12 @@ impl Coordinate {
         let mut num_iter = coor.split(',').map(|c| c.parse::<usize>().unwrap_or_else(|_| panic!("error parsing coordinate: {}", coor)));
         Self(num_iter.next().unwrap_or_else(|| panic!("error getting 1st element in {}", coor)), num_iter.next().unwrap_or_else(|| panic!("error getting 2nd element in {}", coor)), num_iter.next().unwrap_or_else(|| panic!("error getting 3rd element in {}", coor)))
     }
+}
+
+#[test]
+fn check_overlapping() {
+    let b1 = Brick::new_from_str("1,1,1~2,3,1");
+    let b2 = Brick::new_from_str("1,1,2~3,1,2");
+
+    assert!(b1.overlaps(&b2));
 }
